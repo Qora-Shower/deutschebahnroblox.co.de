64939d1b494cffeb515b977864a6fc37eefcc482
DeutscheBahn-Rbx <deutsche.bahn.rbx@gmail.com>
2025-07-13 11:46:57 +0200

    Deine Nachricht

---
+++ login.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+    dots[i].classList.toggle('active', i === index);
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 4000);
