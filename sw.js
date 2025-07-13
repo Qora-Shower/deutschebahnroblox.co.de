2747b4cb1fe480492f9413e60e55c17b1b7435d3
DeutscheBahn-Rbx <deutsche.bahn.rbx@gmail.com>
2025-07-13 16:44:37 +0200

    All

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Deutsche-Bahn-Roblox-v4';
+const CACHE_NAME = 'Deutsche-Bahn-Roblox-v5';
 const BASE = '/deutschebahnroblox.co.de/';  // Basis-Pfad für GitHub Pages
 
 self.addEventListener('install', event => {
